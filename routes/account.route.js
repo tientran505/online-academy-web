bbdcf1aca734cc1b35ae14b528fb96ee8ade1eda
Tien <73010955+tientran505@users.noreply.github.com>
2022-12-19 12:17:44 +0700

    BE: store registered user information

--- routes/account.route.js
+++ routes/account.route.js
@@ -1,8 +1,47 @@
 import express from 'express';
+import User from '../utils/models/user.model.js';
+import bcypt from 'bcryptjs';
+import moment from 'moment';
 
 const router = express.Router();
 
 router.get('/register', (req, res) => {
+  res.render('vwAccount/register', {
+    isPress: true,
+  });
+});
+
+router.get('/valid-user', async(req, res) => {
+  const username = req.query.username;
+  const email = req.query.email;
+
+  const userExist = await User.findOne({username});
+  const emailExist = await User.findOne({email});
+
+  return res.json({
+    userExist: userExist === null,
+    emailExist: emailExist === null,
+  })
+})
+
+router.post('/register', async (req, res) => {
+  console.log(req.body);
+
+  const {username, name, email, dob, password} = req.body;
+
+  const salt = await bcypt.genSalt(10);
+  const hashedPassword = await bcypt.hash(password, salt);
+  const birthday = moment(dob, 'DD/MM/YYYY').format('MM/DD/YYYY');
+
+  const user = await User.create({
+    username,
+    password: hashedPassword,
+    name,
+    email,
+    birthday,
+    role: 'student',
+  })
+
   res.render('vwAccount/register', {
     isPress: true,
   });
