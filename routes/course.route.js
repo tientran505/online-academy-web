fc72c06c6987bdaa574b1210e5013564a8832079
annthuu <80025852+anhthu872@users.noreply.github.com>
2022-12-23 01:19:29 +0700

    pagination

--- routes/course.route.js
+++ routes/course.route.js
@@ -4,10 +4,45 @@ import Course from '../utils/models/course.model.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const p = await Course.find();
+
+  const limit = 6;
+  const curPage = req.query.page || 1;
+  const offset = (curPage - 1)*limit;
+  const total =  await Course.find().count();
+  const nPage = Math.ceil(total/limit);
+  const pageNumber =[];
+  for (let i =1;i <= nPage; i++){
+    pageNumber.push({
+      value:i,
+      isCurrent: i ===+curPage,
+    })
+  }
+  const p = await Course.find().skip(offset).limit(limit);
   const list = JSON.parse(JSON.stringify(p));
+  let prePage;
+  let nextPage;
+  if(1 ===+curPage){
+    prePage =1;
+  }
+  else{
+    prePage = +curPage -1;
+  }
+  if(+nPage===+curPage){
+    nextPage= nPage;
+
+  }
+  else{
+    nextPage = +curPage +1;
+    
+  }
+  console.log(prePage);
+  console.log(nextPage);
   res.render('vwCourse/byCat', {
     course: list,
+    pageNumber: pageNumber,
+    empty: list.length === 0,
+    prePage: prePage,
+    nextPage: nextPage,
   });
 });
 
