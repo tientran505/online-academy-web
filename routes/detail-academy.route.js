47ae7a60035581952aad40d2f56f2514fc2b3a3f
duchuynh1201 <102602729+duchuynh1201@users.noreply.github.com>
2022-12-25 15:18:53 +0700

    Update

--- routes/detail-academy.route.js
+++ routes/detail-academy.route.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import courseModel from '../utils/models/course.model.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('vwDetail/detail-academy');
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  const table = await courseModel.findOne({course_name: 'NodeJS'});
+  const list = JSON.parse(JSON.stringify(table));
+  console.log(list);
+  res.render('vwDetail/detail-academy', {
+    course: list,
+  });
 });
 
 export default router;
