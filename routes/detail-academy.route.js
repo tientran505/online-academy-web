7ee9e4345824a1ed5785e8e58a4335c44a9ffce1
duchuynh1201 <102602729+duchuynh1201@users.noreply.github.com>
2022-12-31 14:51:23 +0700

    Watch video

--- routes/detail-academy.route.js
+++ routes/detail-academy.route.js
@@ -7,10 +7,16 @@ const router = express.Router();
 
 router.get('/:id', async (req, res) => {
   const id = mongoose.Types.ObjectId(req.params.id);
+  
   const table = await courseModel.find({_id: id});
+  const category_list = await courseModel.find({category: table[0].category, _id: {$ne: id}});
+
   const list = JSON.parse(JSON.stringify(table));
+  const list_cate = JSON.parse(JSON.stringify(category_list));
+
   res.render('vwDetail/detail-academy', {
     course: list,
+    cate: list_cate,
   });
 });
 
