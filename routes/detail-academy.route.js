df248cfb254031568863cfd378d9ddedc3fcfd4a
duchuynh1201 <102602729+duchuynh1201@users.noreply.github.com>
2023-01-05 22:31:22 +0700

    Update detail academy

--- routes/detail-academy.route.js
+++ routes/detail-academy.route.js
@@ -13,10 +13,30 @@ router.get('/:id', async (req, res) => {
 
   const list = JSON.parse(JSON.stringify(table));
   const list_cate = JSON.parse(JSON.stringify(category_list));
+  
+  let limit = Math.ceil((list_cate.length > 5 ? 5 : list_cate.length) / 3);
 
+  const slideList = [];
+  let listSlide = [];
+  let listCate = [];
+  let k = 0;
+  for (let i = 0; i < limit; i++) {
+    listCate = [];
+    listSlide = [];
+    let size = list_cate.length - 3 * i > 3 ? 3 : list_cate.length - 3 * i;
+    for (let j = 0; j < size; j++) {
+      list_cate[k].price = parseInt(list_cate[k].price) - parseInt(list_cate[k].sale)
+      listCate.push(
+        list_cate[k],
+      )
+      k++;
+    }
+    listSlide.push({cates: listCate, activeSlide: i === 0});
+    slideList.push({slides: listSlide});
+  }
   res.render('vwDetail/detail-academy', {
     course: list,
-    cate: list_cate,
+    slide: slideList,
   });
 });
 
